6696fb3c323dd9d79e5c5b66a72456f70c58c0bd
Naufhal <naufhalsimdigxrpl2@gmail.com>
2024-05-05 19:40:39 +0700

    ad

--- src/components/Navlink/index.jsx
+++
@@ -1,7 +0,0 @@
-import { Nav } from "react-bootstrap"
-
-export default function SNavlink({ action, children }) {
-    return (
-        <Nav.Link onClick={action}>{children}</Nav.Link>
-    )
-}
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,7 +5,7 @@ import GuestOnlyRoute from "../components/GuestOnlyRoute";
 import Login from "../pages/signin";
 import { HomeRoute } from "./HomeRoute";
 // import { TalentsRoute } from "./TalentsRoute";
-// import { CategoriesRoute } from "./CategoriesRoute";
+import { CategoriesRoute } from "./CategoriesRoute";
 // import { PaymentsRoute } from "./PaymentsRoute";
 // import { EventsRoute } from "./EventsRoute";
 // import { OrdersRoute } from "./OrdersRoute";
@@ -25,8 +25,8 @@ export function AppRoutes() {
       />
       <Route path="/" element={<> <SNavbar /> <GuardRoute /></>}>
         <Route path="dashboard/*" element={<HomeRoute />} />
-        {/* <Route path="categories/*" element={<CategoriesRoute />} />
-        <Route path="talents/*" element={<TalentsRoute />} />
+        <Route path="categories/*" element={<CategoriesRoute />} />
+        {/* <Route path="talents/*" element={<TalentsRoute />} />
         <Route path="payments/*" element={<PaymentsRoute />} />
         <Route path="events/*" element={<EventsRoute />} />
         <Route path="orders/*" element={<OrdersRoute />} /> */}
